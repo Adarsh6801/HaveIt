3a9f580a6135bf92a180316748f4a6c90633b9cd
Adarsh <kpadarsh6801@gmail.com>
2023-03-06 22:35:43 +0530

    food card finished

---
+++ src/app/services/food.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { sample_foods } from 'src/data';
+import { Food } from '../shared/models/food';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAllFood():Food[]{
+    return sample_foods;
+  }
+
+
+}
